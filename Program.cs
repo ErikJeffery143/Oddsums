1f4a57d193bc1571ed61de437c66d1b3ac1493f1
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-10-06 13:11:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6_Percentages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input = 1;
+
+            Console.WriteLine("Enter a NUMBER!!");
+            if (Int32.TryParse(Console.ReadLine(), out input))
+            {
+                for (int i = 1; i < input; i = i + 2)
+                {
+                    Console.WriteLine(Convert.ToString(i));
+                }
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Read better.");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
